840194c46da1761815d704328acd8ccaf7872c1a
Zachary Durr <zdurr22@gmail.com>
2020-09-14 22:56:14 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,12 +1,7 @@
 board = [" "," "," "," "," "," "," "," "," "]
-
-def input_to_index(user_input)
-  user_input.to_i - 1
-end
-
 def turn(board)
   puts "Please enter 1-9:"
-  user_input.gets.strip.to_i
+  user_input = gets.strip.to_i
 end
 
 def display_board(board)
@@ -15,6 +10,10 @@ def display_board(board)
   puts " #{board[3]} | #{board[4]} | #{board[5]} "
   puts "-----------"
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def input_to_index(user_input)
+  user_input.to_i - 1
 end
 
 def move(array, index, value="X")
